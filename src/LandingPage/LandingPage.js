04602d7345b88627c4f5573858bc2b2d5aca35eb
hpanwar521@gmail.com <hpanwar521@gmail.com>
2021-12-28 01:01:39 +0530

    dataget from api

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './landingPage.css';
+import axios from 'axios';
 
 const LandingPage = () => {
+    const [hashtag , setHashtag] = useState('');
+    
+    const apiHandler = async () =>{
+       
+        const res = await axios.get(`https://twitter-sa-api.herokuapp.com/?query=${hashtag}`);
+        
+    }
     return (
         <div class="google-function">
         <form>
         <div>
-          <i class="fas fa-search"> </i> 
+          <i class="fas fa-search" onClick={apiHandler}> </i> 
             <input 
             type="text"
             id="gsearch"
-            name="gsearch"/>
+            name="gsearch"
+            value={hashtag}
+            onChange={(e)=>setHashtag(e.target.value)}/>
         </div>
   
         <div class="button-group">
